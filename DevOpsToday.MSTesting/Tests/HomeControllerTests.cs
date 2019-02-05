447441e6c1fc411ca390327cf914f067d112502c
Herkles <seanpatrickzeitler@gmail.com>
2019-02-05 15:13:41 -0500

    remove final test [#163737478]

--- DevOpsToday.MSTesting/Tests/HomeControllerTests.cs
+++ DevOpsToday.MSTesting/Tests/HomeControllerTests.cs
@@ -21,13 +21,5 @@ namespace DevOpsToday.MSTesting.Tests
       Assert.IsNotNull(sut.Privacy());
     }
 
-    [TestMethod]
-    public void Test_ErrorAction()
-    {
-      var sut = new HomeController();
-
-      Assert.IsNotNull(sut.Error());
-    }
-
   }
 }
