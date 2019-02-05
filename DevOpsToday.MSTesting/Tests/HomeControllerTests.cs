a259f679eb6b3019d19d425082af5129530c701f
Herkles <seanpatrickzeitler@gmail.com>
2019-02-05 14:08:57 -0500

    add test project to solution [#163737478]

---
+++ DevOpsToday.MSTesting/Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DevOpsToday.Controllers;
+
+namespace DevOpsToday.MSTesting.Tests
+{
+  [TestClass]
+  public class HomeControllerTests
+  {
+    [TestMethod]
+    public void Test_IndexAction()
+    {
+      var sut = new HomeController();
+
+      Assert.IsNotNull(sut.Index());
+    }
+
+    [TestMethod]
+    public void Test_privacyAction()
+    {
+      var sut = new HomeController();
+      Assert.IsNotNull(sut.Privacy());
+
+    }
+
+  }
+}
