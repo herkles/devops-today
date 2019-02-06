bc6588e2ef3aa2acb4cf56b436e9041468c5c330
Herkles <seanpatrickzeitler@gmail.com>
2019-02-06 10:21:10 -0500

    fixing code smell of static

--- DevOpsToday/Program.cs
+++ DevOpsToday/Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace DevOpsToday
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
